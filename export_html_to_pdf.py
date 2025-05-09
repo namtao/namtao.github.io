3b69754e0dc6478ff2e39ae55826400a56774410
namtao <vannam.pxb@gmail.com>
2025-05-09 22:18:39 +0700

    Update file 'export_html_to_pdf.py' with new functionality

--- export_html_to_pdf.py
+++ export_html_to_pdf.py
@@ -1,28 +1,3 @@
-# from weasyprint import HTML
-
-# html_file = "index.html"
-# css_file = "style.css"
-# output_pdf = "NguyenVanNam-CV.pdf"
-
-# HTML(html_file).write_pdf(output_pdf, stylesheets=[css_file])
-# print(f"PDF đã được tạo: {output_pdf}")
-
-
-# html_file = "cv-vn.html"
-# css_file = "style-cv.css"
-# output_pdf = "CV_NguyenVanNam-VN.pdf"
-
-# HTML(html_file).write_pdf(output_pdf, stylesheets=[css_file])
-# print(f"PDF đã được tạo: {output_pdf}")
-
-# html_file = "cv-en.html"
-# css_file = "style-cv.css"
-# output_pdf = "CV_NguyenVanNam-EN.pdf"
-
-# HTML(html_file).write_pdf(output_pdf, stylesheets=[css_file])
-# print(f"PDF đã được tạo: {output_pdf}")
-
-
 import asyncio
 from playwright.async_api import async_playwright
 import os
